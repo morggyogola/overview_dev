3844853825ede1515df112a8db707bf7be1dd837
Morgan Ogola <morganogola@gmail.com>
2024-02-15 13:09:36 +0300

    deleted the get users initial router

--- lib/overview_web/router.ex
+++ lib/overview_web/router.ex
@@ -18,13 +18,14 @@ defmodule OverviewWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-    get "/users",PageController, :users
+
   end
 
 
   scope "/api", OverviewWeb do
     pipe_through :api
     resources "/posts", PostController, except: [:new, :edit]
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Other scopes may use custom stacks.
